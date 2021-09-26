f8fb15ac2c4435e2d2bab77c6467e30110793929
Fabian Weik <fabian.weik@protonmail.com>
2021-09-26 19:11:33 +0200

    clean up

--- document_types.py
+++ document_types.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python
 
+from typing import List, Optional
 
-
-from typing import Any, List, Optional
-
-from dataclasses import dataclass, field
-from  dataclasses_json import dataclass_json
+from dataclasses import dataclass, field, asdict
 from datetime import datetime
 import uuid
 from uuid import UUID
 import json
 
 class DateType():
-  @staticmethod
-  def convert_to_remarkable_timestamp(timestamp: float) -> int:
-    return int(timestamp * (10 ** 3))
+    @staticmethod
+    def convert_to_remarkable_timestamp(timestamp: float) -> int:
+        return int(timestamp * (10 ** 3))
 
-  @staticmethod
-  def convert_to_unix_timestamp(r_timestamp: int) -> float:
-    return float(r_timestamp / (10 ** 3))
+    @staticmethod
+    def convert_to_unix_timestamp(r_timestamp: int) -> float:
+        return float(r_timestamp / (10 ** 3))
 
-  @staticmethod
-  def now() -> str:
-    timestamp = datetime.now().timestamp()
-    r_timestamp = DateType.convert_to_remarkable_timestamp(timestamp)
-    return str(r_timestamp)
+    @staticmethod
+    def now() -> str:
+        timestamp = datetime.now().timestamp()
+        r_timestamp = DateType.convert_to_remarkable_timestamp(timestamp)
+        return str(r_timestamp)
 
-@dataclass_json
 @dataclass
-class Document():
-  visibleName: str
-  deleted: bool = False
-  lastModified: str = DateType.now()
-  lastOpened: str = DateType.now()
-  lastOpenedPage: int = 0
-  metamodified: bool = False
-  modified: bool = True
-  parent: str = ""
-  pinned: bool = False
-  synced: bool = False
-  type: str = 'DocumentType'
-  version: int = 0
+class Document(): # pylint: disable=too-many-instance-attributes
+    visibleName: str # pylint: disable=invalid-name
+    deleted: bool = False
+    lastModified: str = DateType.now() # pylint: disable=invalid-name
+    lastOpened: str = DateType.now() # pylint: disable=invalid-name
+    lastOpenedPage: int = 0 # pylint: disable=invalid-name
+    metamodified: bool = False
+    modified: bool = True
+    parent: str = ""
+    pinned: bool = False
+    synced: bool = False
+    type: str = 'DocumentType'
+    version: int = 0
 
 @dataclass
 class DocumentWrapper():
-  document: Document
-  uuid: UUID = uuid.uuid4()
-  child_wrappers: List[Any] = field(default_factory=list)
+    document: Document
+    uuid: UUID = uuid.uuid4()
+    _parent_uuid: Optional[UUID] = None
+    child_uuids: List[UUID] = field(default_factory=list)
 
-  def to_json(self) -> json:
-    return json.loads(self.document.to_json())
+    @property
+    def parent_uuid(self) -> Optional[UUID]:
+        return self._parent_uuid
+
+    @property.setter
+    def parent_uuid(self, parent_uuid: UUID): # pylint: disable=function-redefined
+        self._parent_uuid = parent_uuid
+        self.document.parent = str(parent_uuid)
+
+    def to_json(self) -> json.JSONEncoder:
+        return json.loads(str(asdict(self.document)))
--- upload_zip.py
+++ upload_zip.py
@@ -8,7 +8,6 @@ from os.path import isfile, isdir, join, basename
 from argparse import ArgumentParser
 from zipfile import ZipFile
 from uuid import UUID
-from typing import Union
 
 from document_types import Document, DocumentWrapper
 
@@ -16,13 +15,17 @@ TMP_DIR = '/tmp/reFuse'
 REMARKABLE_DATA_DIR = '/mnt/reMarkable/.local/share/remarkable/xochitl'
 
 def open_zip_notebook(zip_file_name: str) -> DocumentWrapper:
-  notebook_name = zip_file_name.replace('.zip', '')
+    notebook_name = zip_file_name.replace('.zip', '')
 
-  with ZipFile(zip_file_name) as zip_file:
-    zip_file.extractall(TMP_DIR)
+    with ZipFile(zip_file_name) as zip_file:
+        zip_file.extractall(TMP_DIR)
 
-    uuid_str = zip_file.filelist[0].filename
-    uuid_str = re.match(r'[0-9a-f\-]*', uuid_str).group(0)
+        uuid_str = zip_file.filelist[0].filename
+        uuid_match = re.match(r'[0-9a-f\-]*', uuid_str)
+        if uuid_match is None:
+            raise NotImplementedError(f'"{uuid_str}" does not start with a valid UUID')
+
+    uuid_str = uuid_match.group(0)
     uuid = UUID(uuid_str)
 
     document = Document(notebook_name)
@@ -32,49 +35,47 @@ def open_zip_notebook(zip_file_name: str) -> DocumentWrapper:
 
 def upload_document(document_wrapper: DocumentWrapper):
 
-  with open(f'{TMP_DIR}/{document_wrapper.uuid}.metadata', 'w') as metadata_file:
-    json.dump(document_wrapper.to_json(), metadata_file, indent=4)
+    with open(f'{TMP_DIR}/{document_wrapper.uuid}.metadata', 'w') as metadata_file:
+        json.dump(document_wrapper.to_json(), metadata_file, indent=4)
 
-  for filename in listdir(TMP_DIR):
-    if filename.startswith(str(document_wrapper.uuid)):
-      filename = join(TMP_DIR, filename)
+    for filename in listdir(TMP_DIR):
+        if filename.startswith(str(document_wrapper.uuid)):
+            filename = join(TMP_DIR, filename)
 
-      # copy files
-      if isfile(filename):
-        shutil.copy(filename, REMARKABLE_DATA_DIR)
+        # copy files
+        if isfile(filename):
+            shutil.copy(filename, REMARKABLE_DATA_DIR)
 
-      # copy dir
-      elif isdir(filename):
-        src_dir_name = filename
-        dest_dir_name = join(REMARKABLE_DATA_DIR, basename(src_dir_name))
+        # copy dir
+        elif isdir(filename):
+            src_dir_name = filename
+            dest_dir_name = join(REMARKABLE_DATA_DIR, basename(src_dir_name))
 
-        if not isdir(dest_dir_name):
-          makedirs(dest_dir_name)
+            if not isdir(dest_dir_name):
+                makedirs(dest_dir_name)
 
-        for filename in listdir(src_dir_name):
-          filename = join(src_dir_name, filename)
-          shutil.copy(filename, dest_dir_name)
+            for filename in listdir(src_dir_name): # pylint: disable=redefined-outer-name
+                filename = join(src_dir_name, filename)
+                shutil.copy(filename, dest_dir_name)
 
-def register_parent(document_wrapper: DocumentWrapper, parent_dir_uuid: Union[UUID, str]):
-  if type(parent_dir_uuid) is str:
-    parent_dir_uuid = UUID(parent_dir_uuid)
-
-  document_wrapper.document.parent = parent_dir_uuid
+def register_parent(document_wrapper: DocumentWrapper, parent_dir_uuid_str: str):
+    document_wrapper.parent_uuid = UUID(parent_dir_uuid_str)
 
 def main(zip_file_name: str, parent_dir_uuid_str: str):
-  if not zip_file_name.endswith('.zip'):
-    exit(1)
+    if not zip_file_name.endswith('.zip'):
+        raise NotImplementedError(f'"{zip_file_name}" does not end with .zip')
 
-  document_wrapper = open_zip_notebook(zip_file_name)
-  register_parent(document_wrapper, parent_dir_uuid_str)
-  upload_document(document_wrapper)
+    document_wrapper = open_zip_notebook(zip_file_name)
+    register_parent(document_wrapper, parent_dir_uuid_str)
+    upload_document(document_wrapper)
 
 
 if __name__ == "__main__":
-  parser = ArgumentParser(description='Upload a zip file with raw reMarkable notebook data to reMarkable tablet')
-  parser.add_argument('--name', '-n', type=str, required=True)
-  parser.add_argument('--parent-dir', '-p', type=str, default='')
+    parser = ArgumentParser(
+        description='Upload a zip file with raw reMarkable notebook data to reMarkable tablet'
+    )
+    parser.add_argument('--name', '-n', type=str, required=True)
+    parser.add_argument('--parent-dir', '-p', type=str, default='')
 
-  args = parser.parse_args()
-  main(args.name, args.parent_dir)
-
+    args = parser.parse_args()
+    main(args.name, args.parent_dir)
